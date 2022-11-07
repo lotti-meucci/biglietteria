bd01b7289e3f12521bd7537fa6b8f306058a699c
lai2-01 <lai2-01@itismeucci.com>
2022-11-07 10:24:06 +0100

    project setup

---
+++ client/src/main/java/lotti/biglietteria/client/App.java
@@ -0,0 +1,9 @@
+package lotti.biglietteria.client;
+
+public class App
+{
+	public static void main( String[] args )
+	{
+
+	}
+}
---
+++ server/src/main/java/lotti/biglietteria/server/App.java
@@ -0,0 +1,9 @@
+package lotti.biglietteria.server;
+
+public class App
+{
+	public static void main( String[] args )
+	{
+
+	}
+}
